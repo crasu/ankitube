44563c3a4a9715d96bb8f446566f1f03a736c58d
Christian Krämer <spam@crasu.de>
2024-02-09 07:40:56 +0100

    initial version

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import Input from '../lib/input.svelte';
+    let value = '';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<Input bind:value={value}/>
+
+<pre class="status">Value: {value}</pre>
